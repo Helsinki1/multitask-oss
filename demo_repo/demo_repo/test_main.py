a7d9940d409588b4a7b8a06433d80c21a9719bb3
Helsinki1 <davidxionghl@gmail.com>
2026-06-13 14:16:52 -0400

    clear testing repo

--- demo_repo/demo_repo/test_main.py
+++
@@ -1,6 +0,0 @@
-from demo_repo.main import hello_world
-
-def test_hello_world(capsys):
-    hello_world()
-    captured = capsys.readouterr()
-    assert captured.out == "Hello, world!\n"
--- demo_repo/math_utils.py
+++
@@ -1,6 +0,0 @@
-def add(a, b):
-    return a - b   # bug: should be +
-
-
-def multiply(a, b):
-    return a + b   # bug: should be *
